cdaa620e2e0702da7ff42e45d5f76c020cdfd726
Anton Borisov <32795987+boran1999@users.noreply.github.com>
2019-06-27 13:47:57 +0800

    Create adm_form.php

---
+++ adm_form.php
@@ -0,0 +1,18 @@
+<?php
+  include "formclass.php";
+  $base = new formclass;
+ ?>
+<!DOCTYPE HTML>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <title>Test_Form</title>
+ </head>
+ <body bgcolor="lightblue">
+ 	<div>
+ 		<?php
+ 		$base->data_del();
+		?>
+	</div>
+ </body>
+</html>
